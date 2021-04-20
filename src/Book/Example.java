68e9b05108c7c0ee01b8ae12ea02b0b19938583d
LIU  YAN  PING <3132566601@qq.com>
2021-04-20 17:31:59 +0800

    Book

---
+++ src/Book/Example.java
@@ -0,0 +1,30 @@
+package Book;
+
+import Book.AmericanPeople;
+import Book.BeijingPeople;
+import Book.ChinaPeople;
+
+public class Example {
+
+        public static void main(String args[]){
+            ChinaPeople chinaPeople=new ChinaPeople();
+            AmericanPeople americanPeople=new AmericanPeople();
+            BeijingPeople beijingPeople=new BeijingPeople();
+            chinaPeople.speakHello();
+            americanPeople.speakHello();
+            beijingPeople.speakHello();
+            chinaPeople.averageHeight();
+            americanPeople.averageHeight();
+            beijingPeople.averageHeight();
+            chinaPeople.averageWeight();
+            americanPeople.averageWeight();
+            beijingPeople.averageWeight();
+            chinaPeople.chinaGongfu();
+            americanPeople.americanBoxing();
+            beijingPeople.beijingOpera();
+            beijingPeople.chinaGongfu();
+
+        }
+    }
+
+
