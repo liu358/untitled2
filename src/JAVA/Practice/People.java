66e5f6a743ee000589eecdc40ff8205362c99e83
LIU  YAN  PING <3132566601@qq.com>
2021-04-20 15:31:08 +0800

    Practice

---
+++ src/JAVA/Practice/People.java
@@ -0,0 +1,16 @@
+package JAVA.Practice;
+
+public class People {
+    protected double weight,height;
+    public void speakHello(){
+        System.out.println("yayaya");
+    }
+    public void averageHeight(){
+        height=173;
+        System.out.println("average height:"+height);
+    }
+    public void averageWeight(){
+        weight=70;
+        System.out.
+    }
+}
