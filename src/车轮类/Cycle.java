a98a9c8d0189a57013b0fac6a37526743a92b119
LIU  YAN  PING <3132566601@qq.com>
2021-04-25 19:23:27 +0800

    车轮类

---
+++ src/车轮类/Cycle.java
@@ -0,0 +1,46 @@
+package 车轮类;
+
+public class Cycle {
+    public void play() {
+        System.out.println("Cycle");
+    }
+}
+    class Unicycle extends Cycle {
+        public void play() {
+            System.out.println("Unicycle");
+        }
+    }
+
+    class Bicycle extends Cycle {
+        public void play() {
+            System.out.println("Bicycle");
+        }
+    }
+
+    class Tricycle extends Cycle {
+        public void play() {
+            System.out.println("Tricycle");
+        }
+    }
+
+     class Ride {
+        public static void ride(Cycle cycle) {
+            cycle.play();
+        }
+
+        public static void main(String[] args) {
+            Unicycle unicycle = new Unicycle();
+            Bicycle bicycle = new Bicycle();
+            Tricycle tricycle = new Tricycle();
+            ride(unicycle);
+            ride(bicycle);
+            ride(tricycle);
+
+        }
+    }
+
+
+
+
+
+
